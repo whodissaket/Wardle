abaf2b7041f38b42fbc58dcb265022423c3a0e94
mranonymod <mryash018@gmail.com>
2022-03-23 11:08:38 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,18 +1,25 @@
 //@ts-ignore
 const express = require("express");
-//const session = require("express-session");
+const mongodb = require("mongodb");
 const cors = require("cors");
 const router = require('express').Router();
 const dotenv = require("dotenv");
 const asyncHandler = require("express-async-handler");
 dotenv.config();
-console.log(process.env.MONGO_URL)
-const connectdb = require('./config/db');
-connectdb();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+
+mongodb.MongoClient.connect(
+    process.env.MONGO_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    function (err, client) {
+      db = client.db()
+      app.listen(process.env.PORT || 5000, fn);
+    }
+  )
+
 //@ts-ignore
 const post = async  (req, res) => {
     console.log(req.body)
@@ -23,18 +30,41 @@ const get = async (req, res) => {
 }
 //@ts-ignore
 const put = async(req, res) => {
-    console.log(req.body)
+    console.log(req.body.allState)
+    check = await db.collection('wardle').findOne({ deviceID: req.body.allState.deviceID , solution : req.body.allState.solution});
+    console.log(check)
+    if(check){
+        await db.collection('wardle').findOneAndUpdate({ deviceID: req.body.allState.deviceID , solution : req.body.allState.solution},
+            {$set : {guesses:req.body.allState.guesses}},
+            function (err, docs) {
+                if (err){
+                    console.log(err)
+                    res.send(err)
+                }
+                else{
+                    console.log(docs)
+                    res.send('success')
+                }})
+}else {await db.collection("wardle").insertOne({ deviceID: req.body.allState.deviceID , solution : req.body.allState.solution ,guesses:req.body.allState.guesses},
+    function (err, docs) {
+        if (err){
+            console.log(err)
+            res.send(err)
+        }
+        else{
+            console.log(docs)
+            res.send("success")
+        }})}
+
 }
 
 router.post('/',post)
 router.get('/',get)
 router.put('/',put)
-
 app.use('/',router)
 
 
 
 const fn = () => console.log("listening");
 
-app.listen(process.env.PORT || 5000, fn);
 
