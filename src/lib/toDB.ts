d7b1d60468476c97f30cb16371ab3c83560dbfb9
mranonymod <mryash018@gmail.com>
2022-03-23 11:08:26 +0530

    Update toDB.ts

--- src/lib/toDB.ts
+++ src/lib/toDB.ts
@@ -1,11 +1,11 @@
 const axios = require('axios')
-
 export const sendToDB = async (allState : object)=>{
   console.log(allState)
     const { data } = await axios.post(
         `${process.env.REACT_APP_BASE_URL}`,
        { allState },
       )
+      console.log(data)
 }
 export const updToDB =async (allState : object)=>{
   console.log(allState)
@@ -13,9 +13,11 @@ export const updToDB =async (allState : object)=>{
         `${process.env.REACT_APP_BASE_URL}`,
         { allState } ,
       )
+      console.log(data)
 }
 export const getFromDB = async ()=>{
     const { data } = await axios.get(
         `${process.env.REACT_APP_BASE_URL}`,
       )
+      console.log(data)
 }
